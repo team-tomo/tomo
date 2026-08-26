708cf8288161461cd176e1dd2e18dd253666b22c
James Carl Junio <jamescarljunio@gmail.com>
2026-08-26 09:56:08 +0800

    feat: impl checks for github workflows

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -19,4 +19,10 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
+  {
+    files: ["src/components/**/*.{ts,tsx}"],
+    rules: {
+      "react-refresh/only-export-components": "off",
+    },
+  },
 ])
