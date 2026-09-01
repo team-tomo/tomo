69d3c5934cd1c04bc3a7b4b536b337c6ff403fc4
James Carl Junio <jamescarljunio@gmail.com>
2026-09-01 17:22:08 +0800

    fix: failing github checks

--- apps/web/src/routes/auth/reset-password.tsx
+++ apps/web/src/routes/auth/reset-password.tsx
@@ -1,6 +1,6 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router"
 
-export const Route = createFileRoute('/auth/reset-password')({
+export const Route = createFileRoute("/auth/reset-password")({
   component: RouteComponent,
 })
 
